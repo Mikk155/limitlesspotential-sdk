10ee33183f4fda144072b4d98633d0f318d9bb14
Mikk155 <martimsanchez155@gmail.com>
2025-03-14 20:47:57 -0300

    Remove CSharp bin/obj before compiling

--- src/FGDGenerator/FGDGenerator.bat
+++ src/FGDGenerator/FGDGenerator.bat
@@ -1,8 +1,9 @@
 @echo off
+
+if exist bin rmdir /s /q bin
+if exist obj rmdir /s /q obj
+
 setlocal
 dotnet add package Newtonsoft.Json
 dotnet run
 endlocal
-
-if exist bin rmdir /s /q bin
-if exist obj rmdir /s /q obj
